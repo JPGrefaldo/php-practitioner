62608cb3b57e9ae1f7f3677e8f2e10f59e49c3f8
JPGrefaldo <johnpaulgrefaldo@gail.com>
2018-09-18 19:43:34 +0800

    PDO non-class

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+try {
+    $pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', 'root');
+} catch (PDOException $e) {
+    die('Could not connect');
+}
+
+$statement = $pdo->prepare('select * from todos');
+
+$statement->execute();
+
+$tasks = $statement->fetchAll(PDO::FETCH_OBJ);
+
+// var_dump($results[0]->description);
+
+require 'index.view.php';
