1a7ca0a78758916af4b039f2b93a461766abe6b4
JPGrefaldo <johnpaulgrefaldo@gail.com>
2018-09-18 20:53:40 +0800

    PDO full-class

--- functions.php
+++ functions.php
@@ -5,19 +5,3 @@ function dd($data) {
     die(var_dump($data));
     echo '</pre>';
 }
-
-function connectToDb() {
-    try {
-        return new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', 'root');
-    } catch (PDOException $e) {
-        die('Could not connect');
-    }
-}
-
-function fetchAllTask($pdo) {
-    $statement = $pdo->prepare('select * from todos');
-
-    $statement->execute();
-
-    return $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
-}
--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
 
-require 'functions.php';
+$query = require 'bootstrap.php';
 require 'Task.php';
 
-$pdo = connectToDb();
-
-$tasks = fetchAllTask($pdo);
+$tasks = $query->selectAll('todos');
 
 require 'index.view.php';
